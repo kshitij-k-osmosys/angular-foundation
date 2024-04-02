ab937d8e0b211c8b637536dcbaaff676456f6a51
kshitij-k-osmosys <kshitij-k-osmosys@users.noreply.github.com>
2024-04-02 09:21:35 +0000

    deploy: 832491b29f3a482189734b919e03f1b918924dab

--- assets/i18n/loader.ts
+++
@@ -1,8 +0,0 @@
-import { HttpClient } from '@angular/common/http';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-
-function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http, '../assets/i18n/', '.json');
-}
-
-export default HttpLoaderFactory;
